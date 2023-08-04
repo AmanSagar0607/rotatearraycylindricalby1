8a27a1869d661539be974d909a4558b3aad3b3fb
Aman Sagar <luckysagar6265@gmail.com>
2023-08-04 23:37:24 +0530

    add sum file

---
+++ gitSample/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter value of a:" << endl;
+    cin >> a;
+    cout << "enter value of b:" << endl;
+       cin >> b;
+    int sum = a + b;
+    cout << "sum is " << sum << endl;
+    return 0;
+}
